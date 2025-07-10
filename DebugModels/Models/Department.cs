5deb71d773f7fd3036cbff3afbf2a61addbf12e5
mani <mani.84.zmn@gmail.com>
2025-07-11 01:05:44 +0330

    feat: Create Model Department and PreRegs and edit another model Exist

---
+++ DebugModels/Models/Department.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Runtime.CompilerServices;
+
+namespace DebugModels.Models
+{
+    public class Department
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [MaxLength(50)]
+        [Required]
+        public string Name { get; set; }
+
+        public string Building { get; set; }
+
+        public decimal Budget { get; set; }
+
+        #region
+        public List<Instructor> Instructors { get; set; }
+
+        public List<Student> Students { get; set; }
+
+        public List<Course> Courses { get; set; }
+        #endregion
+    }
+}
---
+++ DebugModels/Models/PreRegs.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace DebugModels.Models
+{
+    public class PreRegs
+    {
+        public int Id { get; set; }
+
+
+        #region
+        public int? PreRegCourseId { get; set; }
+        [ForeignKey("PreRegCourseId")]
+        public Course? PreRegCourse { get; set; }
+
+        public int? CoureId { get; set; }
+        [ForeignKey("CoureId")]
+        public Course Course { get; set; }
+        #endregion
+
+    }
+}
